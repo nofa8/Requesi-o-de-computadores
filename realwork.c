9e9aab135bf72b93a72f9a18445e19ee63d58ca2
goncaloR7 <118633930+goncaloR7@users.noreply.github.com>
2022-12-15 17:38:36 +0000

    Create realwork.c
    15/12/2022

---
+++ realwork.c
@@ -0,0 +1,324 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+#define MAXX 30
+#define UM 1
+#define RAM 32
+#define I3 "i3"
+#define I7 "i7"
+#define I5 "i5"
+#define MAX_NOME 80
+#define ZERO 0
+#define MENOR_ANO 2014
+#define MAIOR_ANO 2021
+#define MAX_DESIG 20
+#define MAX_REQ 9999
+
+//Structures for storing the information!
+typedef struct{
+    //Basic Structure for storing the DAY of something happening!
+    int dy;
+    char mnth[15];
+    int yer;
+}DATa;
+
+
+typedef struct{
+    char requiscode[15];
+    DATa requis;
+    int numpraz;     //prazo days
+    DATa praz;      // prazo requisicao
+    //state of a pc?
+    //estado de requesição
+    char statreq[16];
+
+    //Devolução
+    DATa devol;
+    char local[15];
+    float finee;
+
+    //UTENTE STUFF
+    char typeut[16];
+    int numutent;
+    char nomedoutente[MAX_NOME];
+}REQUi;
+
+
+typedef struct{
+    REQUi *requisition;
+    int portId;
+    char desig[MAX_DESIG];
+    char process[12];
+    int  memoria;
+    char stat[20];
+    int quantAvarias;
+    int quantReq;
+    char local[15];
+    DATa aquis;
+    float valuequi;
+}PORTATIl;
+
+typedef struct{
+    int portatexist;        // Portateis existentes
+    int portatedisp;        //Portateis disponiveis
+    int totalderequisefet;  // Total de requisiçoes feitas
+    int requisativas;       // Requisiçoes ativas
+} CONTADOREs;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+void limpaBufferStdin(void);
+int lerInteiro(int min, int max);
+void lerString(char vetor[], int max);
+float lerFloat(float min, float max);
+void systemClear(void);
+
+
+char menu(CONTADOREs *cont, PORTATIl port[MAXX]);
+void subListar(CONTADOREs *cont, PORTATIl port1[MAXX]);
+void lePortate(CONTADOREs *cont, PORTATIl port2[MAXX]);
+void listaReq(CONTADOREs *cont, PORTATIl port[MAXX]);
+void insertPCs(CONTADOREs *cont, PORTATIl port[MAXX]);
+
+
+int main()
+{
+    PORTATIl portateis[30];
+    CONTADOREs oscontadores;
+    char escolha;
+    //portateis->requisition = malloc(10*sizeof(REQUi));
+    oscontadores.portatexist = 0;
+    oscontadores.portatedisp = 0;
+    oscontadores.requisativas = 0;
+    oscontadores.totalderequisefet = 0;
+
+    do{
+        escolha = menu(&oscontadores, portateis);
+        switch(escolha){
+            case 'A':
+
+                break;
+            case 'I':
+                insertPCs(&oscontadores, portateis);
+                break;
+            case 'M':
+
+                break;
+            case 'G':
+//                gravaFicheiroTexto(vEstudantes,quantInscritos);
+//                gravaFicheiroBinario(vEstudantes, quantInscritos);
+                break;
+            case 'L':
+                subListar(&oscontadores, portateis);
+                break;
+            case 'S':
+                printf("\nA sair ...\n");
+            break;
+        }
+    }while(escolha != 'S');
+
+
+
+    return 0;
+}
+
+
+
+char menu(CONTADOREs *cont, PORTATIl port[MAXX]){
+    char opcao;
+    printf("\n\n==========================Menu Principal==========================\n\n");
+    printf("\tQuantidade de portateis: %d", cont->portatexist);
+    printf("\t\tQuantidade de portateis disponiveis: %d \n\tTotal de requisicoes efetuadas: %d\tQuantidade de requisicoes ativas: %d\n", cont->portatedisp, cont->totalderequisefet, cont->requisativas);
+
+    printf("\nI - Inserir Portateis\n");
+    printf("L - Listar Informacao\n\n");      //adicionar dados de requisicoes
+
+    printf("R - Registar Requisicao\n");
+    printf("V - Registar Avaria / Reparacao\n");
+    printf("D - Registar Devolucao \n\n");
+
+    printf("A - Alteracao da Localizacao de um portatil\n");
+    printf("Y - Renovar requisicao\n");
+    printf("M - Mostrar dados de Requisicoes\n\n");
+
+    printf("G - Gravar dados nos Ficheiros\n");
+    printf("E - Extrair dados dos Ficheiros\n\n");
+    printf("Z - Apresentacao de dados estatisticos\n");
+    printf("\tS - Sair\n\t\t\tOpcao --> ");
+
+    do{
+        opcao = getchar();
+        limpaBufferStdin();
+        opcao = toupper(opcao);
+        if(opcao != 'M'&& opcao != 'Y'&& opcao != 'Z'&& opcao != 'A'&& opcao != 'D'&& opcao != 'V'&& opcao != 'I'&& opcao != 'L' && opcao != 'G' && opcao != 'E' && opcao != 'R' && opcao != 'S'){
+            printf("Opcao invalida!\n\n");
+        }
+    }while(opcao != 'M'&& opcao != 'Y'&& opcao != 'Z'&& opcao != 'A'&& opcao != 'D'&& opcao != 'V'&& opcao != 'I'&& opcao != 'L' && opcao != 'G' && opcao != 'E' && opcao != 'R' && opcao != 'S');
+
+
+
+    return opcao;
+}
+void subListar(CONTADOREs *cont, PORTATIl port1[MAXX]){     //passar quantPortateis nos ()
+    int i;
+    char opcao1;
+
+    printf("\n\n\n\n\n\n\n\n==========================Listar Informacao==========================\n\n");
+    printf("\tP - Informacao de Portateis\n\n");
+    printf("\tT - Listar dados de Todas Requisicoes\n\n");
+    printf("\tR - Apresentar informacao de uma requisicao\n\n");
+    printf("\tA - Apresentar listagem de todas as avarias ocorridas\n\n");
+    printf("\tS - Sair\n");
+    printf("\t\tOpcao: ");
+    do{
+        opcao1 = getchar();
+        limpaBufferStdin();
+        opcao1 = toupper(opcao1);
+        if(opcao1 != 'P'&& opcao1 != 'T'&& opcao1 != 'R'&& opcao1 != 'A' && opcao1 != 'S'){
+            printf("\nOpcao invalida!\nInsira Novamente: ");
+        }
+    }while(opcao1 != 'P'&& opcao1 != 'T'&& opcao1 != 'R'&& opcao1 != 'A' && opcao1 != 'S');
+
+    switch(opcao1){
+    case 'P':
+        lePortate(cont,port1);
+        break;
+    case 'T':
+        //listaReq(cont,port1);
+        break;
+    case 'R':
+        break;
+    case 'A':
+        break;
+    case 'S':
+        printf("\n\t.\n");
+        break;
+    }
+ }
+
+void lePortate(CONTADOREs *cont, PORTATIl port2[MAXX]){
+    int i;
+    if(cont->portatexist!=0){
+        for(i=0; i < cont->portatexist; i++){
+            printf("\nId: %d\n", port2[i].portId);
+            printf("Portatil %s: \n", port2[i].desig);
+            printf("\tProcessador: %s", port2[i].process);
+            printf("\tEstado: %s\n", port2[i].stat);
+            printf("\tLocalizacao: %s", port2[i].local);
+            printf("\tData de aquisicao: %d / %d / %d\n", port2[i].aquis.dy,port2[i].aquis.mnth,port2[i].aquis.yer);
+            printf("\tValor do equipamento: %f", port2[i].valuequi);
+            printf("\tQuantidade de avarias: %d\n", port2[i].quantAvarias);
+            printf("\tQuantidade de requisicoes: %d\n", port2[i].quantReq);
+            printf("\tTipo de utente: %15c\tPrazo de requisicao: %d dias", port2[i].requisition->typeut,port2[i].requisition->numpraz);
+        }
+    }
+    else{
+        printf("\nNao ha computadores!\n");
+    }
+}
+
+void listaReq(CONTADOREs *cont, PORTATIl port[MAXX]){
+
+
+
+
+
+}
+
+
+void insertPCs(CONTADOREs *cont, PORTATIl port[MAXX]){
+    int i,u;
+    printf("\nQuantos pcs deseja inserir: ");
+    u = lerInteiro(1,MAXX);
+    u += cont->portatexist;
+    if(u>30){
+        printf("Nao e possivel adicionar mais computadores.\n");
+    }
+    else{
+        for(i = (*cont).portatexist; i<u; i++){
+            printf("\nId: ");
+//            lerID(cont,port);
+            printf("\nDesignacao do portatil: ");
+            printf("\n\tProcessador: ");
+            printf("\n\tEstado: ");
+            printf("\n\tLocalizacao: ");
+            printf("\n\tData de aquisicao: ");
+            printf("\n\tValor do equipamento: ");
+
+        }
+    }
+
+
+}
+
+
+
+/*_________________________________________________________________________________________________*/
+
+void limpaBufferStdin(void){
+    char lixo;
+    do
+    {
+        lixo=getchar();
+    }
+    while (lixo!='\n' && lixo!=EOF);
+}
+
+int lerInteiro(int min, int max){
+    int numero, controlo;
+    do
+    {
+        controlo = scanf("%d", &numero);// scanf devolve quantidade de valores válidos obtidos
+        limpaBufferStdin(); /* elimina caracteres que ficaram no buffer stdin */
+        if (numero<min || numero>max || controlo==0){
+            printf("Valor invalido! \nInsira novamente: ");
+        }
+    }
+    while (numero<min || numero>max || controlo==0);
+    return numero;
+}
+
+float lerFloat(float min, float max)
+{
+    float numero;
+    int controlo;
+    do{
+        controlo = scanf("%f", &numero); // scanf devolve quantidade de valores válidos obtidos
+        limpaBufferStdin(); /* elimina caracteres que ficaram no buffer stdin */
+    }while (numero<min || numero>max || controlo==0);
+    return numero;
+}
+
+void lerString(char vetor[], int max)
+{
+    int tamanhoString;
+    do  /*Repete leitura caso sejam introduzidas strings apenas com o \n (tamanhoString == 1) */
+    {
+        fgets(vetor, max, stdin);
+        tamanhoString = strlen(vetor);
+    }
+    while(tamanhoString == 1);
+    if (vetor[tamanhoString-1] != '\n'){
+        /* armazenados no vetor todos os caracteres */
+        limpaBufferStdin(); /* elimina caracteres que ficaram no buffer stdin */
+    }
+    else {
+        vetor[tamanhoString-1] ='\0'; /*substitui \n da string armazenada em vetor por \0 */
+    }
+}
+
